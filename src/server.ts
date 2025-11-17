9a73de86da1455656cd5fc4275caef05ab4ca6d4
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2025-11-17 20:40:58 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { json } from "body-parser";
-import router from "./webhook";
+import router from "./webhook.js";
 
 const app = express();
-
 app.use(json());
 app.use("/webhook", router);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
