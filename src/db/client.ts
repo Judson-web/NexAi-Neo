c703272ac813b330f201727c77131ebdfb4e25db
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2025-11-18 08:17:40 +0530

    Update client.ts

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,14 +1,14 @@
+// src/db/client.ts
+
 import { createClient } from "@supabase/supabase-js";
 import { config } from "../config.js";
 
 export const supabase = createClient(
   config.supabase.url,
-  config.supabase.service,
-  { auth: { persistSession: false } }
-);
-
-export const supabasePublic = createClient(
-  config.supabase.url,
-  config.supabase.anon,
-  { auth: { persistSession: false } }
+  config.supabase.key,
+  {
+    auth: {
+      persistSession: false
+    }
+  }
 );
